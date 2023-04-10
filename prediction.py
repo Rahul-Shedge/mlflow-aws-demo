35d5da878235eb415dfe62a0d306c6b809be3c76
Rahul-Shedge <rahulshedge555@outlook.com>
2023-04-11 02:00:07 +0530

    minor changes

--- prediction.py
+++ prediction.py
@@ -5,9 +5,13 @@ import pandas as pd
 
 
 
-data = pd.read_json('{"fixed acidity":{"0":7.4},"volatile acidity":{"0":0.7},"citric acid":{"0":0},\
-                    "residual sugar":{"0":1.9},"chlorides":{"0":0.076},"free sulfur dioxide":{"0":11},\
-                    "total sulfur dioxide":{"0":34},"density":{"0":0.9978},"pH":{"0":3.51},"sulphates":{"0":0.56},"alcohol":{"0":9.4} \
+data = pd.read_json('{"fixed acidity":{"0":7.4},"volatile acidity":{"0":0.7},\
+                    "citric acid":{"0":0},\
+                    "residual sugar":{"0":1.9},"chlorides":{"0":0.076},\
+                    "free sulfur dioxide":{"0":11},\
+                    "total sulfur dioxide":{"0":34}, \
+                    "density":{"0":0.9978},"pH":{"0":3.51},\
+                    "sulphates":{"0":0.56},"alcohol":{"0":9.4} \
                     }')
 
 if __name__ == "__main__":
@@ -16,4 +20,3 @@ if __name__ == "__main__":
     sagemaker = query(input_json)
     Response = sagemaker.query(input_json)
     print(f"Prediction From Model Endpoint :{Response}")
-    
