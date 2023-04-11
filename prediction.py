75408dab2c1caed693569de8023819d8a241b2a3
Rahul-Shedge <rahulshedge555@outlook.com>
2023-04-11 17:03:18 +0530

    minor change

--- prediction.py
+++ prediction.py
@@ -6,8 +6,8 @@ import json
 
 
 
-#data = pd.read_json('{"fixed acidity":{"0":7.4},"volatile acidity":{"0":0.7},"citric acid":{"0":0},"residual sugar":{"0":1.9},"chlorides":{"0":0.076},"free sulfur dioxide":{"0":11},"total sulfur dioxide":{"0":34}, "density":{"0":0.9978},"pH":{"0":3.51},"sulphates":{"0":0.56},"alcohol":{"0":9.4} }')
-data = pd.read_csv("test.csv",sep=";")
+data = pd.read_json('{"fixed acidity":{"0":7.4},"volatile acidity":{"0":0.7},"citric acid":{"0":0},"residual sugar":{"0":1.9},"chlorides":{"0":0.076},"free sulfur dioxide":{"0":11},"total sulfur dioxide":{"0":34}, "density":{"0":0.9978},"pH":{"0":3.51},"sulphates":{"0":0.56},"alcohol":{"0":9.4} }')
+#data = pd.read_csv("test.csv",sep=";")
 #input = json.dumps(data.to_json())
 
 
